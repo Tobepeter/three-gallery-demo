d895b3dde647caf84ef801f63a6964bcbba7207f
tobe <34265941+Tobepeter@users.noreply.github.com>
2024-07-27 01:51:43 +0800

    chore: add hamburger

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -5,17 +5,17 @@ import { FileAddOutlined, FileOutlined, InboxOutlined } from '@ant-design/icons'
 import { CheckboxOptionType, Radio, UploadProps } from 'antd';
 
 export const Home: FC = () => {
-  const mock_url = 'models/dog.webp';
-  const mock_glb = 'models/dog.glb';
-
-  const mock_url2 = 'models/girl.webp';
-  const mock_glb2 = 'models/girl.glb';
+  // const mock_url = 'models/dog.webp';
+  // const mock_glb = 'models/dog.glb';
+  const mock_items = ['dog', 'girl', 'hamburger'];
 
   const [selectedModel, setSelectedModel] = useState<ModelData | null>(null);
 
   const data: ModelData[] = Array.from({ length: 20 }).map((_, i) => {
-    const cover = i % 2 === 0 ? mock_url : mock_url2;
-    const model = i % 2 === 0 ? mock_glb : mock_glb2;
+    const itemName = mock_items[i % mock_items.length];
+    const cover = `models/${itemName}.webp`;
+    const model = `models/${itemName}.glb`;
+
     return {
       id: i.toString(),
       cover,
@@ -25,7 +25,7 @@ export const Home: FC = () => {
 
   return (
     // <div className="p-3 bg-violet-300 min-h-lvh">
-    <div className="p-3 bg-gradient-to-r from-violet-300 via-violet-400 to-violet-300 min-h-lvh">
+    <div className="p-3 bg-gradient-to-r from-violet-200 via-violet-300 to-violet-200 min-h-lvh">
       <div className="text-3xl text-violet-900">model gallery</div>
       {/* TODO: add flex wrap anim when have time */}
       <Flex wrap style={{ marginTop: 12 }} gap="small">
